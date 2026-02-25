918d2ad342102ce745962c9a1cb285770960fcc6
Dustin <dustinmeng@Dustins-MacBook-Pro.local>
2026-02-24 22:51:27 -0600

    updates function so it also returns the cards that contributed to scoring

--- src/HandEval.hs
+++ src/HandEval.hs
@@ -1,40 +1,50 @@
 module HandEval where
 
 import Types
-import Data.List (sort, group, sortBy)
+import Data.List (sort, group, sortBy, maximumBy)
 
 
 
--- Evaluates the 5 card hand
-eval :: [Card] -> HandType
+-- Evaluates the 5 card hand, returns the handtype and the cards contribute to the handtype
+eval :: [Card] -> (HandType, [Card])
 eval cards
-  | isRoyaleFlush    = RoyaleFlush
-  | isStraightFlush = StraightFlush
-  | isFourOfAKind   = FourOfAKind
-  | isFullHouse     = FullHouse
-  | isFlush         = Flush
-  | isStraight      = Straight
-  | isThreeOfAKind  = ThreeOfAKind
-  | isTwoPair       = TwoPair
-  | isOnePair       = OnePair
-  | otherwise       = HighCard
-  where 
-    ranks = map rank cards 
-    suits = map suit cards 
-    sorted = sort ranks 
-    grouped = group sorted 
-    counts = map length grouped 
+  | isRoyaleFlush   = (RoyaleFlush,   cards)
+  | isStraightFlush = (StraightFlush, cards)
+  | isFourOfAKind   = (FourOfAKind,   getScoringCards 4)
+  | isFullHouse     = (FullHouse,     cards)
+  | isFlush         = (Flush,         cards)
+  | isStraight      = (Straight,      cards)
+  | isThreeOfAKind  = (ThreeOfAKind,  getScoringCards 3)
+  | isTwoPair       = (TwoPair,       getScoringCards 2)
+  | isOnePair       = (OnePair,       getScoringCards 2)
+  | otherwise       = (HighCard,      [highestCard])
+  where
+    ranks = map rank cards
+    suits = map suit cards
+    sorted = sort ranks
+    grouped = group sorted
+    counts = map length grouped
     groups = sortBy (flip compare) counts
-    -- checks if the hand is flush
+
     isFlush = all (== head suits) (tail suits)
     isStraight = isConsecutive sorted || sorted == [Two, Three, Four, Five, Ace]
     isStraightFlush = isFlush && isStraight
     isRoyaleFlush = isFlush && sorted == [Ten, Jack, Queen, King, Ace]
-    isFourOfAKind = groups == [4,1]
-    isFullHouse    = groups == [3, 2]
+    isFourOfAKind = groups == [4, 1]
+    isFullHouse = groups == [3, 2]
     isThreeOfAKind = groups == [3, 1, 1]
-    isTwoPair      = groups == [2, 2, 1]
-    isOnePair      = groups == [2, 1, 1, 1]
+    isTwoPair = groups == [2, 2, 1]
+    isOnePair = groups == [2, 1, 1, 1]
+
+    -- Get cards whose rank appears exactly n times
+    getScoringCards n =
+      let scoringRanks = [head g | g <- grouped, length g == n]
+      in filter (\c -> rank c `elem` scoringRanks) cards
+
+    
+
+    -- Highest card
+    highestCard = maximumBy (\a b -> compare (rank a) (rank b)) cards
 
 -- check if a sorted list of ranks is consecutive
 isConsecutive :: [Rank] -> Bool
