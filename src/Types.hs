fd396900dcd99e4280880f3c02fd3dcdaf769733
Dustin <dustinmeng@Dustins-MacBook-Pro.local>
2026-02-24 22:17:04 -0600

    source implementation: defining all required types except jokers

---
+++ src/Types.hs
@@ -0,0 +1,54 @@
+module Types where
+
+import Data.List (sort, group, sortBy)
+import Data.Ord (Down(..))
+
+-- Card suits
+data Suit = Clubs | Diamonds | Hearts | Spades
+  deriving (Eq, Ord, Enum, Bounded, Show, Read)
+
+-- Card ranks from low to high
+data Rank 
+  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+  | Jack | Queen | King | Ace
+  deriving (Eq, Ord, Enum, Bounded, Show, Read)
+
+-- Card
+data Card = Card
+  { rank :: Rank
+  , suit :: Suit
+  } 
+  deriving (Eq, Show, Read)
+
+-- Hand type ranked from worst to best
+data HandType
+  = HighCard
+  | OnePair
+  | TwoPair
+  | ThreeOfAKind
+  | Straight
+  | Flush
+  | FullHouse
+  | FourOfAKind
+  | StraightFlush
+  | RoyaleFlush
+  deriving (Eq, Ord, Enum, Bounded, Show, Read)
+
+-- Hand result
+data HandResult = HandResult
+  { handType :: HandType
+  , baseChips :: Int
+  , multiplier :: Int
+  }
+  deriving (Eq, Show)
+
+-- | Current state of the game
+data GameState = GameState
+  { deck         :: [Card]
+  , currentHand  :: [Card]
+  , score        :: Int
+  , targetScore  :: Int
+  , handsLeft    :: Int
+  , discardsLeft :: Int
+  } 
+  deriving (Show)
